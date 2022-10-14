2a585b8f37519750bdea3f17bfb779cdd69048f8
Ash Luchowa <ashwinluchowa@gmail.com>
2022-10-14 12:24:48 +0800

    Cleanups + guide comment

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-let pScore = 9;
-let cScore = 10;
+//Global variables
+let pScore = 0;
+let cScore = 0;
 
+//Target variables
 const buttons = document.querySelectorAll('.main-button button');
-const rockBtn = document.getElementById('rock-btn');
-const paperBtn = document.getElementById('paper-btn');
-const scissorBtn = document.getElementById('scissor-btn');
 const mainDisplay = document.querySelector('.main-display p');
 const winDisplay = document.querySelector('.main-display h2');
 const computerDisplay = document.querySelector('.computer-display span');
 const playerDisplay = document.querySelector('.player-display span');
+//Target Buttons
+const rockBtn = document.getElementById('rock-btn');
+const paperBtn = document.getElementById('paper-btn');
+const scissorBtn = document.getElementById('scissor-btn');
 
-computerDisplay.textContent = cScore;
+//Displays
 playerDisplay.textContent = pScore;
-
+computerDisplay.textContent = cScore;
 mainDisplay.textContent = 'Choose your destiny';
 
 //Computer random choice [rock, paper or scissor]
@@ -32,11 +35,13 @@ rockBtn.addEventListener('click', () => {
     } else if (computerSelection === 'paper') {
         winDisplay.textContent = 'You lost!';
         mainDisplay.textContent = `The enemy chose ${computerSelection}. Paper wraps rock!`;
+        computerDisplay.textContent = ++cScore;
     } else {
         winDisplay.textContent = 'You won!';
         mainDisplay.textContent = `The enemy chose ${computerSelection}. Rock breaks scissor!`;
+        playerDisplay.textContent = ++pScore;
     }
-})
+});
 
 //Player selects paper
 paperBtn.addEventListener('click', () => {
@@ -46,14 +51,16 @@ paperBtn.addEventListener('click', () => {
     if (computerSelection === 'rock') {
         winDisplay.textContent = 'You won!';
         mainDisplay.textContent = `The enemy chose ${computerSelection}. Paper wraps rock!`;
+        playerDisplay.textContent = ++pScore;
     } else if (computerSelection === 'paper') {
         winDisplay.textContent = 'It\'s a tie!';
         mainDisplay.textContent = `The enemy chose ${computerSelection} too.`;
     } else {
         winDisplay.textContent = 'You lost!';
         mainDisplay.textContent = `The enemy chose ${computerSelection}. Scissor cuts paper!`;
+        computerDisplay.textContent = ++cScore;
     }
-})
+});
 
 //Player selects scissor
 scissorBtn.addEventListener('click', () => {
@@ -63,11 +70,21 @@ scissorBtn.addEventListener('click', () => {
     if (computerSelection === 'rock') {
         winDisplay.textContent = 'You lost!';
         mainDisplay.textContent = `The enemy chose ${computerSelection}. Rock breaks scissor!`;
+        computerDisplay.textContent = ++cScore;
     } else if (computerSelection === 'paper') {
         winDisplay.textContent = 'You won!';
         mainDisplay.textContent = `The enemy chose ${computerSelection}. Scissor cuts paper!`;
+        playerDisplay.textContent = ++pScore;
     } else {
         winDisplay.textContent = 'It\'s a tie!';
         mainDisplay.textContent = `The enemy chose ${computerSelection} too`;
     }
-})
+});
+
+//Find another way to click through all 3 buttons
+
+//Add playRound function(playerSelection, computerSelection) {
+    //move game logics here
+//}
+
+//addEventListeners - when click, store value in the playerSelection variable
