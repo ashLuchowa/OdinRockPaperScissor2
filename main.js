b814653c4a1c43bf84ff393ffd0d698f64cc6149
Ash Luchowa <ashwinluchowa@gmail.com>
2022-10-14 09:42:43 +0800

    Cleanup computer selection + playerSelection + playRound

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
 let pScore;
 let cScore;
+let playerSelection;
+let computerSelection
+const buttons = document.querySelectorAll('.main-button button');
+const rockBtn = document.getElementById('rock-btn');
+const paperBtn = document.getElementById('paper-btn');
+const scissorBtn = document.getElementById('scissor-btn');
 
-//Computer random choice [rock, paper & scissor]
-const getComputerChoice = Math.floor(Math.random() * 3) +1;
-getComputerChoice === 1 ? 'rock' :
-getComputerChoice === 2 ? 'paper' : 'scissor';
+//Computer random choice [rock, paper or scissor]
+const getComputerChoice = Math.floor(Math.random() * 3) + 1;
+if(getComputerChoice === 1) {
+    computerSelection = 'rock';
+} else if(getComputerChoice === 2) {
+    computerSelection = 'paper';
+} else {
+    computerSelection = 'scissor';
+}
+
+//Player select [rock, paper or scissor]
+rockBtn.addEventListener('click', () => playerSelection = 'rock');
+paperBtn.addEventListener('click', () => playerSelection = 'paper');
+scissorBtn.addEventListener('click', () => playerSelection = 'scissor');
+
+
+//Play 1 round
+let playRound = (playerSelection, computerSelection) => {
+
+    //computer chooses rock
+    if(computerSelection === 'rock') {
+        if(playerSelection === 'rock') {
+            alert('tie');
+        } else if(playerSelection === 'paper') {
+            alert('you won');
+        } else {
+            alert('You lost');
+        }
+    }
+}
+
+playRound();
